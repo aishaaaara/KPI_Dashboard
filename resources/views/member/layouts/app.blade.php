60525d100099d02d104a0eabb47315c8ee07aae4
aishaaaara <aisharahmadia8@gmail.com>
2026-06-27 23:17:12 +0700

    Perbaikan Tab icon

--- resources/views/member/layouts/app.blade.php
+++ resources/views/member/layouts/app.blade.php
@@ -5,7 +5,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="{{ asset('images/logo1.jpeg') }}">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('site.webmanifest') }}">
     <title>KPI Dashboard</title>
 
     {{-- Bootstrap CSS --}}
